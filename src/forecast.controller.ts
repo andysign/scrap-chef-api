bd75d9e08206aaba34a1df2085c0539d9858bd4e
andysign <andysignerdesigner@gmail.com>
2025-07-02 15:44:58 +0100

    Moved swagger decorators info to ext variables

--- src/forecast.controller.ts
+++ src/forecast.controller.ts
@@ -1,10 +1,33 @@
 import { Controller, Get, Query } from "@nestjs/common";
 import { ApiTags, ApiOperation, ApiQuery, ApiResponse } from "@nestjs/swagger";
+import { ApiQueryOptions, ApiResponseOptions } from "@nestjs/swagger";
 import { ForecastService } from "./forecast.service";
 import { json2csv as j2c } from "csv42";
 import * as c2md from "csv-to-markdown-table";
 
-// import { ForecastDataDto } from "./dto/forecast-data.dto";
+const GetForecastApiQueryFmt: ApiQueryOptions = {
+  name: "fmt",
+  required: false,
+  description: "Set to 'csv' / 'md' to retrieve in the respective format.",
+  enum: ["csv", "md"],
+};
+const GetForecastApiQueryGrade: ApiQueryOptions = {
+  name: "grade",
+  required: true,
+  description: "The grade to forecast",
+  example: "B500A",
+};
+const GetForecastApiResponse: ApiResponseOptions = {
+  status: 200,
+  description: "Historical data followed by a one-month forecast.",
+  schema: {
+    type: "array",
+    items: {
+      type: "array",
+      example: [],
+    },
+  },
+};
 
 @ApiTags("Forecast")
 @Controller("")
@@ -13,38 +36,13 @@ export class ForecastController {
 
   @Get("/prod/forecast")
   @ApiOperation({ summary: "Get hist months followed by and a forecasted one" })
-  @ApiQuery({
-    name: "fmt",
-    required: false,
-    description: "Set to 'csv' / 'md' to retrieve in the respective format.",
-    enum: ["csv", "md"],
-  })
-  @ApiQuery({
-    name: "grade",
-    required: true,
-    description: "The grade to forecast",
-    example: "B500A",
-  })
-  @ApiResponse({
-    status: 200,
-    description: "Historical data and a one-month forecast.",
-    // type: "object",
-    // type: [ForecastDataDto],
-    schema: {
-      type: "array",
-      items: {
-        type: "array",
-        example: [],
-      },
-    },
-  })
-  getForecast(
-    @Query("fmt") f: string,
-    @Query("grade") grade: string,
-  ): Promise<any[] | string> {
+  @ApiQuery(GetForecastApiQueryFmt)
+  @ApiQuery(GetForecastApiQueryGrade)
+  @ApiResponse(GetForecastApiResponse)
+  getFr(@Query("fmt") f: any, @Query("grade") g: any): Promise<any[] | string> {
     if (f === "csv" || f === "md") {
       return new Promise(async (res) => {
-        const data = await this.forecastService.getForecast(grade);
+        const data = await this.forecastService.getForecast(g);
         if (f === "csv") {
           res(j2c(data));
         } else {
@@ -52,6 +50,6 @@ export class ForecastController {
         }
       });
     }
-    return this.forecastService.getForecast(grade);
+    return this.forecastService.getForecast(g);
   }
 }
