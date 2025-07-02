f4df51e680ff095a3d7430c338a6905ae07a0e51
andysign <andysignerdesigner@gmail.com>
2025-07-02 11:06:14 +0100

    Added hard coded initial production data

--- src/app.js
+++ src/app.js
@@ -3,8 +3,23 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const initialProductionData = `
+Year 	, Month 	, Grade   	, Batches 	
+2024 	, 6     	, B500A   	, 119     	
+2024 	, 6     	, A36     	, 9       	
+2024 	, 6     	, C35     	, 4       	
+2024 	, 6     	, A53/A53 	, 4       	
+2024 	, 7     	, B500A   	, 119     	
+2024 	, 7     	, A36     	, 9       	
+2024 	, 7     	, C35     	, 4       	
+2024 	, 7     	, A53/A53 	, 4       	
+2024 	, 8     	, B500A   	, 1       	
+2024 	, 8     	, A36     	, 5       	
+2024 	, 8     	, C35     	, 3       	
+2024 	, 8     	, A53/A53 	, 5       	
+`;
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
