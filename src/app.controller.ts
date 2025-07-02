25c921634efb0ca12bc22573e730d2d48a737c6e
andysign <andysignerdesigner@gmail.com>
2025-07-02 12:04:18 +0100

    Added swagger details and decorators for /prod/data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { ProductionDataDto } from "./dto/production-data.dto";
+import { ApiOperation, ApiQuery, ApiResponse } from "@nestjs/swagger";
 
 @Controller()
 export class AppController {
@@ -7,11 +9,29 @@ export class AppController {
 
   @Get("/")
   getHello() {
-    return this.appService.getHello();
+    return "NestJs API. Go to /api/v0/ pls.";
   }
 
-  @Get("prod/data")
-  getProdData() {
+  @Get("/prod/data")
+  @ApiOperation({ summary: "Get the complete production data" })
+  @ApiQuery({
+    name: "fmt",
+    required: false,
+    description: "Set to 'csv' or 'tsv' to receive data in CSV or TSV format.",
+    enum: ["csv", "tsv"],
+  })
+  @ApiResponse({
+    status: 200,
+    description: "A list of production data records.",
+    type: [ProductionDataDto],
+  })
+  getProdData(@Query("fmt") f: string): Promise<ProductionDataDto[] | string> {
+    console.log({ f });
+    if (f == "csv") {
+      return new Promise((resolve) => resolve("foo"));
+    } else if (f == "tsv") {
+      return new Promise((resolve) => resolve("var"));
+    }
     return this.appService.getProdData();
   }
 
