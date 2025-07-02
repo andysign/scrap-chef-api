0253536f3fc41d4d88d550911bbf2000a0728c38
andysign <andysignerdesigner@gmail.com>
2025-07-02 15:38:51 +0100

    Made the forecast processRows fn more generic and added dummy forecastAndAppendToRow

--- src/forecast.service.ts
+++ src/forecast.service.ts
@@ -4,29 +4,42 @@ import { Database } from "sqlite3";
 
 const convCoefficient = 100;
 const batchesToTonsConv = (b: number) => Math.floor(b * convCoefficient);
-const tonsToBatchesConv = (b: number) => Math.floor(b / convCoefficient);
+//    tonsToBatchesConv = (b: number) => Math.floor(b / convCoefficient);
 
 @Injectable()
 export class ForecastService {
   constructor(@Inject("DATABASE_CONNECTION") private db: Database) {}
 
-  private processRows(rows: any[], grade: string) {
-    const g = grade;
-    const key = "SteelGrade_" + g;
+  private processRows(rows: any[], grade: any, i: any) {
+    const key = "Grade_" + i + "_" + grade;
     const processedData: any[] = rows.map((row: any) => ({
       DateYearAndMonth: `${row.year}-${String(row.month).padStart(2, "0")}`,
       Forecast: "N",
       // Grade: grade,
       [key]: row.batches,
-      // Tons: batchesToTonsConv(row.batches),
     }));
 
     if (rows.length > 0) {
-      const lastMonth = rows[rows.length - 1];
-      const nextMonth = new Date(lastMonth.year, lastMonth.month, 1);
-      nextMonth.setMonth(nextMonth.getMonth());
       return processedData;
     }
+    return [];
+  }
+
+  private forecastAndAppendToRows(rows: any, k: any) {
+    if (rows.length === 0) return [];
+    const key = k;
+    const forecastBatches = Math.round(
+      rows.reduce((acc: any, cur: any) => acc + cur[key], 0) / rows.length,
+    );
+    const last = rows[rows.length - 1];
+    const date = new Date(last.DateYearAndMonth);
+    const newDate = new Date(date.setMonth(date.getMonth() + 1));
+    rows.push({
+      DateYearAndMonth: `${newDate.getFullYear()}-${String(newDate.getMonth() + 1).padStart(2, "0")}`,
+      Forecast: "Y",
+      [key]: forecastBatches,
+    });
+    return rows;
   }
 
   getForecast(grade: string): Promise<any[]> {
@@ -47,10 +60,9 @@ export class ForecastService {
         if (err) return reject(err);
         if (rows.length === 0) return resolve([]);
 
-        const processedData = this.processRows(rows, grade);
-
-        console.log(Object.entries(processedData));
-        resolve(processedData);
+        const i = 0;
+        const processedPr = this.processRows(rows, grade, i);
+        resolve(processedPr);
       });
     });
   }
