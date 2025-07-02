7f82ac567223e6d6fbcdbd96faac7c66ee0fd8ea
andysign <andysignerdesigner@gmail.com>
2025-07-02 11:49:55 +0100

    Added databse module and provider

---
+++ src/database.providers.ts
@@ -0,0 +1,29 @@
+import * as sqlite3 from "sqlite3";
+
+export const databaseProviders = [
+  {
+    provide: "DATABASE_CONNECTION",
+    useFactory: async () => {
+      const db = new sqlite3.Database(":memory:");
+      db.serialize(() => {
+        db.run(`
+          CREATE TABLE production_data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            year INTEGER,
+            month INTEGER,
+            grade TEXT,
+            batches INTEGER
+          );
+        `);
+        db.run(`
+          CREATE TABLE groups_data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            grade TEXT NOT NULL,
+            group_name TEXT NOT NULL
+          );
+        `);
+      });
+      return db;
+    },
+  },
+];
