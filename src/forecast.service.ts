1921d33c1cf35276a560743c960b0a561b57ec59
andysign <andysignerdesigner@gmail.com>
2025-07-02 15:43:20 +0100

    Added separate function for average based forecast

--- src/forecast.service.ts
+++ src/forecast.service.ts
@@ -15,30 +15,43 @@ export class ForecastService {
     const processedData: any[] = rows.map((row: any) => ({
       DateYearAndMonth: `${row.year}-${String(row.month).padStart(2, "0")}`,
       Forecast: "N",
-      // Grade: grade,
       [key]: row.batches,
     }));
 
-    if (rows.length > 0) {
-      return processedData;
-    }
+    if (rows.length > 0) return processedData;
     return [];
   }
 
-  private forecastAndAppendToRows(rows: any, k: any) {
+  private forecastWithAverage(rows: any[], grade: string): number {
+    const totalBatches = rows.reduce((acc, cur) => acc + (cur[grade] || 0), 0);
+    const forecastBatches = Math.round(totalBatches / rows.length);
+    return forecastBatches;
+  }
+
+  private forecastAndAppendToRows(rows: any[]) {
     if (rows.length === 0) return [];
-    const key = k;
-    const forecastBatches = Math.round(
-      rows.reduce((acc: any, cur: any) => acc + cur[key], 0) / rows.length,
-    );
+
+    const firstRow = rows[0];
+    const keys = Object.keys(firstRow).filter((k) => k.startsWith("Grade_"));
+
+    if (keys.length === 0) return rows;
+
     const last = rows[rows.length - 1];
     const date = new Date(last.DateYearAndMonth);
     const newDate = new Date(date.setMonth(date.getMonth() + 1));
-    rows.push({
-      DateYearAndMonth: `${newDate.getFullYear()}-${String(newDate.getMonth() + 1).padStart(2, "0")}`,
+    const newDateY = newDate.getFullYear();
+    const newDateM = newDate.getMonth();
+
+    const forecastRow: { [key: string]: any } = {
+      DateYearAndMonth: `${newDateY}-${String(newDateM + 1).padStart(2, "0")}`,
       Forecast: "Y",
-      [key]: forecastBatches,
-    });
+    };
+
+    for (const key of keys) {
+      forecastRow[key] = this.forecastWithAverage(rows, key);
+    }
+
+    rows.push(forecastRow);
     return rows;
   }
 
@@ -46,9 +59,7 @@ export class ForecastService {
     return new Promise((resolve, reject) => {
       const sql = `
         SELECT
-            year,
-            month,
-            batches
+            year, month, batches
         FROM
             production_data
         WHERE
@@ -62,7 +73,8 @@ export class ForecastService {
 
         const i = 0;
         const processedPr = this.processRows(rows, grade, i);
-        resolve(processedPr);
+        const withForecast = this.forecastAndAppendToRows(processedPr);
+        resolve(withForecast);
       });
     });
   }
